c7c141da0d216e3344526e763da3bd2342546dfa
Your Name <you@example.com>
2022-09-21 11:19:18 +0530

    Code fix by amar

--- Prime_Problem.js
+++ Prime_Problem.js
@@ -1,9 +1,21 @@
 function check_prime(num){
+    let factors=0;
     for(let i=1;i<=num;i++){
-        if(num%i){
-            return true;
+        if(num%i===0){
+            factors++;
         }
-        return false;
+    }
+    if(factors===2){
+        return true;
+    }else{
+	  return false;
     }
 }
-check_prime(17);
+
+let ans = check_prime(17);
+
+if(ans===true){
+   console.log("Prime");
+}else{
+   console.log("Not Prime");
+}
