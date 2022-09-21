5e80874fd00354349dd08953980e373d272a16e4
Aman Kumar <amanmandal644@gmail.com>
2022-09-21 11:56:19 +0530

    Code fix by aman

--- Prime_Problem.js
+++ Prime_Problem.js
@@ -1,9 +1,20 @@
-function check_prime(num){
-    for(let i=1;i<=num;i++){
-        if(num%i){
-            return true;
+function check_prime(number){
+    let factors=0;
+    for(let i=1;i<=number;i++){
+        if(number%i===0){
+            factors++;
         }
+    }
+    if(factors===2){
+        return true;
+    }else{
         return false;
     }
 }
-check_prime(17);
+let answer = check_prime(17);
+
+if(answer===true){
+    console.log("Prime");
+}else{
+    console.log("Not Prime");
+}
